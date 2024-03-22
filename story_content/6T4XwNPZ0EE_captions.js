5c004889c29555cbd8d20dfb1821c723b8cb7ee0
mleblanc24 <164504913+mleblanc24@users.noreply.github.com>
2024-03-22 12:30:22 -0400

    Add files via upload

---
+++ story_content/6T4XwNPZ0EE_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.66.28270.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:06.324%0D%0ASelf-Check%0D%0AAnswer%20each%20question,%20then%20click%20the%20%22See%20Response%22%20button%20%0D%0A%0D%0A"}')
